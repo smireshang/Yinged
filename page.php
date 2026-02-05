6d4795f86a58101a429b8a716fec1c30f8879070
Ida bully <30439000+smireshang@users.noreply.github.com>
2026-02-05 18:04:36 +0800

    优化头部布局并加粗文章列表标题

--- page.php
+++ page.php
@@ -1,13 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-<nav class="nav">
-<ul class="flat">
-<li class="active"><a href="<?php $this->options->siteUrl(); ?>">首页</a></li>
-    <?php $this->widget('Widget_Contents_Page_List')
-               ->parse('<li class="active"><a href="{permalink}">{title}</a></li> '); ?></ul>
-                </nav>
-            </div>
         <h1 class="post-title" itemprop="name headline">
             <?php $this->title() ?>
         </h1>
