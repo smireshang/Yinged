fb997a32d3b3da6747f6f411486a7857a3c7aafd
Ida bully <30439000+smireshang@users.noreply.github.com>
2026-02-05 20:04:42 +0800

    切换为顶部三段音频条律动过渡

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 $(function () {
+    const body = $('body');
     const button = $('#to_top');
+
+    $('a[href]').on('click', function (event) {
+        const href = $(this).attr('href');
+        const isHash = !href || href.indexOf('#') === 0;
+        const isJs = href && href.indexOf('javascript:') === 0;
+        const isDownload = $(this).attr('download') !== undefined;
+        const isModifiedClick = event.metaKey || event.ctrlKey || event.shiftKey || event.altKey || event.which === 2;
+        const isExternal = this.hostname && this.hostname !== window.location.hostname;
+
+        if (isHash || isJs || isDownload || isModifiedClick || this.target || isExternal) {
+            return;
+        }
+
+        event.preventDefault();
+        body.addClass('page-loading');
+        window.setTimeout(function () {
+            window.location.href = href;
+        }, 180);
+    });
 
     if (!button.length) {
         return;
     }
 
     const toggleButton = function () {
-        if ($(window).scrollTop() > 30) {
-            button.stop(true, true).fadeIn(150);
+        if ($(window).scrollTop() > 200) {
+            button.stop(true, true).fadeIn(200);
         } else {
-            button.stop(true, true).fadeOut(150);
+            button.stop(true, true).fadeOut(200);
         }
     };
 
@@ -19,7 +39,7 @@ $(function () {
     $(window).on('scroll', toggleButton);
 
     button.on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 800);
+        $('html, body').animate({ scrollTop: 0 }, 500);
         return false;
     });
 });
