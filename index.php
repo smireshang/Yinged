e17285371e0852cc7ac137a47aa794d269753b1d
Ida bully <30439000+smireshang@users.noreply.github.com>
2026-02-06 14:57:28 +0800

    优化遮罩与正文懒加载并新增首页置顶功能

--- index.php
+++ index.php
@@ -10,20 +10,45 @@
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $this->need('header.php');
+
+$topCids = yingedGetTopCids($this->options->topCids);
+$isFirstPage = isset($this->_currentPage) ? ((int) $this->_currentPage === 1) : true;
+$showTopPosts = $isFirstPage && !empty($topCids);
 ?>
 
-            <article>              
-            <div class="recent-posts section">
-  <h2 class="section-header">随笔<i class="ri-quill-pen-line"></i></h2>
-  <?php while ($this->next()): ?>
-    <div class="posts"><div class="post">
-    <a href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-    <div class="time"><?php $this->date('F j, Y'); ?></div></div><div class="post">
-    <?php endwhile; ?>
-</ul>
+<article>
+    <div class="recent-posts section">
+        <h2 class="section-header">随笔<i class="ri-quill-pen-line"></i></h2>
+
+        <?php if ($showTopPosts): ?>
+            <?php foreach ($topCids as $topCid): ?>
+                <?php $this->widget('Widget_Archive@top-' . $topCid, 'pageSize=1&type=post', 'cid=' . $topCid)->to($topPost); ?>
+                <?php if ($topPost->have()): $topPost->next(); ?>
+                    <div class="posts">
+                        <div class="post is-top-post">
+                            <a href="<?php $topPost->permalink(); ?>"><span class="top-flag">[TOP]</span><?php $topPost->title(); ?></a>
+                            <div class="time"><?php $topPost->date('F j, Y'); ?></div>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
+
+        <?php while ($this->next()): ?>
+            <?php if ($showTopPosts && in_array((int) $this->cid, $topCids, true)) continue; ?>
+            <div class="posts">
+                <div class="post">
+                    <a href="<?php $this->permalink(); ?>"><?php $this->title(); ?></a>
+                    <div class="time"><?php $this->date('F j, Y'); ?></div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    </div>
 </article>
+
 <ul class="pagination">
-    <li class="page-item page-previous"><?php $this->pageLink('下一页','next'); ?><span aria-hidden="true"></span></a></li>     
-    <li class="page-item page-next"><?php $this->pageLink('上一页'); ?><span aria-hidden="true"></span></a></li>
+    <li class="page-item page-previous"><?php $this->pageLink('下一页', 'next'); ?></li>
+    <li class="page-item page-next"><?php $this->pageLink('上一页'); ?></li>
 </ul>
+
 <?php $this->need('footer.php'); ?>
